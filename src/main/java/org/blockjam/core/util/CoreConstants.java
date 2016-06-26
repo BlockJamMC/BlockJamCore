6af549f33008befb55f6478e099c7d2536d144fa
Max Roncace <me@caseif.net>
2016-06-26 14:04:50 -0400

    Add CoreConstants class and define UI colors

---
+++ src/main/java/org/blockjam/core/util/CoreConstants.java
@@ -0,0 +1,45 @@
+/*
+ * This file is part of BlockJamCore, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016, BlockJam <https://blockjam.org/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.blockjam.core.util;
+
+import org.spongepowered.api.text.format.TextColor;
+import org.spongepowered.api.text.format.TextColors;
+
+/**
+ * Static utility class for storing constant values.
+ */
+public class CoreConstants {
+
+    /**
+     * Contains colors for uniformity throughout UI.
+     */
+    public static final class Color {
+        public static final TextColor INFO = TextColors.DARK_AQUA;
+        public static final TextColor CONFIRM = TextColors.GREEN;
+        public static final TextColor ALERT = TextColors.RED;
+        public static final TextColor EM = TextColors.GOLD;
+    }
+
+}
